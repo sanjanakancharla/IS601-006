9b13608fc45ae190bbcce1915a318bcc47fd7290
sanjkn <sanjana3839@gmail.com>
2023-04-23 23:30:22 -0400

    added changes

--- Project/auth/auth.py
+++ Project/auth/auth.py
@@ -1,22 +1,20 @@
 from flask import Blueprint, render_template, flash, redirect, url_for,current_app, session
 from auth.forms import LoginForm, ProfileForm, RegisterForm
-from accounts.accounts import get_or_create_account
 from sqll.db import DB
 
 from flask_login import login_user, login_required, logout_user, current_user
 from auth.models import User
-from accounts.models import Account
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
 from flask_principal import Identity, AnonymousIdentity, \
      identity_changed
+from email_validator import validate_email
 
 auth = Blueprint('auth', __name__, url_prefix='/',template_folder='templates')
 
 def check_duplicate(e):
-
     import re
     r = re.match(".*IS601_Users.(\w+)", e.args[0].args[1])
     if r:
@@ -41,6 +39,7 @@ def register():
                 flash("Successfully registered","success")
         except Exception as e:
             check_duplicate(e)
+            #flash(str(e), "danger")
     return render_template("register.html", form=form)
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -49,6 +48,18 @@ def login():
     if form.validate_on_submit():
         is_valid = True
         email = form.email.data # email or username
+        if "@" in email:
+            try:
+                validate_email(email)
+            except:
+                is_valid = False
+                flash("Invalid email address", "danger")
+        else:
+            import re
+            r = re.fullmatch("/^[a-z0-9_-]{2,30}$/", email)
+            if r:
+                is_valid = False
+                flash("Invalid username", "danger")
         password = form.password.data
         if is_valid:
             try:
@@ -67,11 +78,6 @@ def login():
                             print("role rows", result.rows)
                             user.roles = [Role(**r) for r in result.rows]
                         print(f"Roles: {user.roles}")
-                        # getting or creating account here so we can lazy populate
-                        # existing users (prior to this feature implementation)
-                        account = get_or_create_account(user.id)
-                        user.account = Account(**account)
-
                         success = login_user(user) # login the user via flask_login
                         
                         if success:
@@ -123,28 +129,44 @@ def profile():
         is_valid = True
         email = form.email.data
         username = form.username.data
+        import re
+        r = re.fullmatch('^[a-z0-9_-]{2,30}$', username)
+        if r is None:
+            is_valid = False
+            flash("Invalid username", "danger")
+        print(r)
         current_password = form.current_password.data
         password = form.password.data
         confirm = form.confirm.data
+        # print('username:',username)
+        # print('email:', email)
+        # print('current_password:',current_password)
+        # print('password:',password)
+        # print('confirm:',confirm)
+        
         # handle password change only if all 3 are provided
         if current_password and password and confirm:
-            try:
-                result = DB.selectOne("SELECT password FROM IS601_Users where id = %s", user_id)
-                if result.status and result.row:
-                    # verify current password
-                    if bcrypt.check_password_hash(result.row["password"], current_password):
-                        # update new password
-                        hash = bcrypt.generate_password_hash(password)
-                        try:
-                            result = DB.update("UPDATE IS601_Users SET password = %s WHERE id = %s", hash, user_id)
-                            if result.status:
-                                flash("Updated password", "success")
-                        except Exception as ue:
-                            flash(ue, "danger")
-                    else:
-                        flash("Invalid password","danger")
-            except Exception as se:
-                flash(se, "danger")
+            if password != confirm:
+                is_valid = False
+                flash("Passwords do not match", "danger")
+            else:
+                try:
+                    result = DB.selectOne("SELECT password FROM IS601_Users where id = %s", user_id)
+                    if result.status and result.row:
+                        # verify current password
+                        if bcrypt.check_password_hash(result.row["password"], current_password):
+                            # update new password
+                            hash = bcrypt.generate_password_hash(password)
+                            try:
+                                result = DB.update("UPDATE IS601_Users SET password = %s WHERE id = %s", hash, user_id)
+                                if result.status:
+                                    flash("Updated password", "success")
+                            except Exception as ue:
+                                flash(ue, "danger")
+                        else:
+                            flash("Invalid password","danger")
+                except Exception as se:
+                    flash(se, "danger")
         
         if is_valid:
             try: # update email, username (this will trigger if nothing changed but it's fine)
@@ -158,11 +180,7 @@ def profile():
         result = DB.selectOne("SELECT id, email, username FROM IS601_Users where id = %s", user_id)
         if result.status and result.row:
             user = User(**result.row)
-            # switch how user is loaded so we don't lose error validations
-            # form = ProfileForm(obj=user)
-            print("loading user", user)
-            form.username.data = user.username
-            form.email.data = user.email
+            form = ProfileForm(obj=user)
             # TODO update session
             current_user.email = user.email
             current_user.username = user.username
